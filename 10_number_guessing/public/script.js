119d0a194c106c6cf5ec95c0f9b4dedcdb664e86
Etsija <jyrki.keisala@gmail.com>
2023-03-30 17:02:23 +0300

    Fix the game logic.

--- 10_number_guessing/public/script.js
+++ 10_number_guessing/public/script.js
@@ -12,42 +12,55 @@ function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
   }
   
+  function reset() {
+    currentNumber = getRandomInt(min, max);
+    currentMin = min;
+    currentMax = max;
+    updateInfo(min, max);
+    updateResult("");
+    let guess_input = document.getElementById("guess");
+    guess_input.value = "";
+}
+
 // Update the info txt
 function updateInfo(min, max) {
     console.log("min:" + min + " max:" + max);
-    let resultTxt = document.getElementById("result");
+    let resultTxt = document.getElementById("info");
     resultTxt.innerHTML = "Choose a number between " + min + " and " + max + ".";
 }
 
-function reset() {
-    currentNumber = getRandomInt(min, max);
-    updateInfo(min, max);
-    console.log(currentNumber);
+// Update the guess result
+function updateResult(result) {
+    let infoTxt = document.getElementById("guess_result");
+    infoTxt.innerHTML = result;
 }
 
 function submit() {
-    let resultTxt = document.getElementById("result");
     let guess_input = document.getElementById("guess");
     let guess = guess_input.value;
+    console.log(currentNumber);
 
     if (guess == "") {
-        resultTxt.innerHTML = "Not a valid number";
+        updateResult("Input is not a valid number!");
+        updateInfo(currentMin, currentMax);
         return; 
     } else if (guess < currentMin || guess > currentMax) {
-        resultTxt.innerHTML = "Valid area = [" + currentMin + ", " + currentMax + "]";
+        updateResult("Input is out of bounds!");
+        updateInfo(currentMin, currentMax);
         return;
     } else if (guess == currentNumber) {
-        resultTxt.innerHTML = "YOU WON! The number was " + currentNumber;
+        updateResult("YOU WON! The number was " + currentNumber + ".");
+        updateInfo(0, 100);
         return;
     } else if (guess < currentNumber) {
+        updateResult("The number I'm thinking of is BIGGER.");
         currentMin = guess;
         updateInfo(currentMin, currentMax);
         return;
     } else if (guess > currentNumber) {
+        updateResult("The number I'm thinking of is SMALLER.");
         currentMax = guess;
         updateInfo(currentMin, currentMax);
         return;
     } 
-    
-    console.log("valid: " + guess);
 }
